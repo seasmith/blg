9e4251989bfc03f918a8bfe8e5d4b87e66e4f7e3
Luke Smith <lukedansmi@sbcglobal.net>
2017-11-28 00:15:57 -0600

    Having a state lookup table will be so useful

--- R/data.R
+++ R/data.R
@@ -3,4 +3,12 @@
 #' Colors that I like to use when creating charts.
 #'
 #' @format A list of colors.
-"blg_colors"
+"clrs"
+
+#' State lookup tibble.
+#'
+#' This sure seems useful when you need to match a state's abbreviation with its
+#' full name, and vice-versa
+#'
+#' @format A US state lookup table of names and abbreviations
+"state_lookup"
